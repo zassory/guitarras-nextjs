ff5c7bbec83b524681bfd133c66efffbdae3fb9d
Nicolas <nicolas.programador@gmail.com>
2022-11-29 13:00:56 -0300

    getServerSideProps terminado, recoge parametro correctamente

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -1,13 +1,27 @@
 //import { useRouter } from 'next/router';
 
-const Producto = () => {
+const Producto = ({ guitarra }) => {
 
+  console.log(guitarra[0].attributes.nombre);
   //const router = useRouter();
   //console.log(router);
 
   return (
     <div>[url]</div>
   )
+}
+
+export const getServerSideProps = async( { query: { url } } ) => {
+  
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`);
+  const { data: guitarra } = await respuesta.json();  
+
+  return {
+    props: {
+      guitarra
+    }
+  }
+
 }
 
 export default Producto
